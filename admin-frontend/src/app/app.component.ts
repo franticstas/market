dbd208f3f32f824f2bf7d6b602fedf772ee34778
franticstas <franticstas@gmail.com>
2022-04-10 22:34:26 +0300

    add user router

--- admin-frontend/src/app/app.component.ts
+++ admin-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'admin-frontend';
+  constructor(private http: HttpClient) { }
+
+  send() {
+    const user = {
+      name: 'Test name',
+      email: 'rrrr@sss.ru'
+    }
+    
+    this.http.post('/api/users', user).subscribe()
+    this.http.get('/api/test').subscribe()
+  }
 }
